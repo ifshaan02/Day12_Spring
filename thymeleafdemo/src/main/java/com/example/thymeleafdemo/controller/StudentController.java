e4f4cf826835278e6d052cf2dc40de09988636f9
ifshaan02 <ifshaan02@gmail.com>
2025-09-16 21:39:08 +0530

    Add files via upload

---
+++ thymeleafdemo/src/main/java/com/example/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,57 @@
+package com.example.thymeleafdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.thymeleafdemo.model.Student;
+
+@Controller
+public class StudentController {
+	
+	@Value("${countries}")
+	private List<String> countries;
+	
+	@Value("${languages}")
+	private List<String> languages;
+	
+	@Value("${systems}")
+	private List<String> systems;
+
+	@GetMapping("/showStudentForm")
+	public String showForm(Model theModel) {
+		
+		//create student obj
+		Student theStudent = new Student();
+		
+		
+		//add student obj to the model
+		theModel.addAttribute("student", theStudent);
+		
+		// add the list of countries to the model
+		theModel.addAttribute("countries",countries);
+		
+		// add the list of language to the model
+		theModel.addAttribute("languages",languages);
+		
+		// add the list of systems to the model
+		theModel.addAttribute("systems", systems);
+		
+		return "student-form";
+	}
+	
+	
+	@PostMapping("/processStudentForm")
+	public String processForm(@ModelAttribute("student") Student theStudent) {
+		
+		// log the input data
+		System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+		
+		return "student-confirmation";
+	}	
+}
